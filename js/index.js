010c4c89a023f72a361e9a77029e0f0e3ea24c11
Jamie Fraser <jamierobertfraser@gmail.com>
2016-08-02 12:55:35 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+var modalImg = document.getElementById("img01");
+var imgs = document.getElementsByClassName("jamie-img");
+
+var captionText = document.getElementById("caption");
+
+for (var i = 0; i < imgs.length; i++) {
+  var img = imgs[i];
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+  }
+}
