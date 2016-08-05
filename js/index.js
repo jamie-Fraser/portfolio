9a6f0f5760517380398d9b3143fadedbebcc1398
Jamie Fraser <jamierobertfraser@gmail.com>
2016-08-05 10:13:55 +0800

    jquery event

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
-var modal = document.getElementById('myModal');
+$( document ).ready(function() {
+	
+  $('a[href$="index.html"]').on('click', function(event) {
+		$('.below-head').remove();
+		event.preventDefault();
+	});
+	var modal = document.getElementById('myModal');
 
-var span = document.getElementsByClassName("close")[0];
+	var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+	span.onclick = function() {
+		modal.style.display = "none";
+	
+	}
+	var modalImg = document.getElementById("img01");
+	var imgs = document.getElementsByClassName("jamie-img");
 
-var modalImg = document.getElementById("img01");
-var imgs = document.getElementsByClassName("jamie-img");
+	var captionText = document.getElementById("caption");
 
-var captionText = document.getElementById("caption");
-
-for (var i = 0; i < imgs.length; i++) {
-  var img = imgs[i];
-  img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    modalImg.alt = this.alt;
-    captionText.innerHTML = this.alt;
-  }
-}
+	for (var i = 0; i < imgs.length; i++) {
+		var img = imgs[i];
+		img.onclick = function() {
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			modalImg.alt = this.alt;
+			captionText.innerHTML = this.alt;
+		}
+	}
+})
