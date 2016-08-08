2116eb71130a346bc8c4233a10ffb8e5eb8b8ede
Jamie Fraser <jamierobertfraser@gmail.com>
2016-08-08 14:10:50 +0800

    gneeeh

--- js/index.js
+++ js/index.js
@@ -1,21 +1,11 @@
 $( document ).ready(function() {
 	
-  $("a[href='portfolio.html']").on('click', function(event) {
+  $("a[href='.html']").on('click', function(event) {
 		$('.home').remove();
 		event.preventDefault();
 		$(".belowHead").load('portfolio.html .portfolio');
 	});
 	
-	$("a[href='contact.html']").on('click', function(event) {
-		$('.home').remove();
-		event.preventDefault();
-		$(".belowHead").load('contact.html .contact');
-	});
-	$("a[href='index.html']").on('click', function(event) {
-		$('.home').remove();
-		event.preventDefault();
-		$(".belowHead").load('index.html .home');
-	});
 
 		$('a.gallery').featherlightGallery({
     previousIcon: '&#9664',
