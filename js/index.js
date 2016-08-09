9166feb9c8715e1130e37e766f4ae818ea06a09a
Jamie Fraser <jamierobertfraser@gmail.com>
2016-08-09 12:08:20 +0800

    backstretch

--- js/index.js
+++ js/index.js
@@ -15,14 +15,29 @@ $(document).ready(function() {
 	$("a[href='index.html']").on('click', function(event) {
 		$('.home').remove();
 		event.preventDefault();
-		$(".belowHead").load('index.html .home');
+		$(".belowHead").load('index.html');
 	});
 
 		$('a.gallery').featherlightGallery({
-    previousIcon: '&#9664',
-    nextIcon: '&#965',
+    previousIcon: '&#10092',
+    nextIcon: '&#10093',
     galleryFadeIn: 300,
 
     openSpeed: 300
 	});
+	
+	$('.my-gallery').featherlightGallery({
+    filter: "a",
+    afterContent: function() {
+		this.$legend = this.$legend || $('<div class="legend"/>').insertAfter(this.$content);
+		this.$legend.text(this.$currentTarget.attr('alt'));
+		
+		$(".foo").backstretch([
+			"home/jamie/web/portfolio/Portfolio Images/Digital/Portraits/rick.jpg",
+			"home/jamie/web/portfolio/Portfolio Images/Digital/Portraits/tayah.jpg",
+			"home/jamie/web/portfolio/Portfolio Images/Digital/Portraits/tyrion.jpg"    
+			], {duration: 4000});
+    }
+	});
 })
+
