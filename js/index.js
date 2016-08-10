f48d1e36d7e4e9a889a4559d39c499fed3c8f52d
Jamie Fraser <jamierobertfraser@gmail.com>
2016-08-10 14:04:07 +0800

    backstretch slideshow

--- js/index.js
+++ js/index.js
@@ -1,42 +1,43 @@
 $(document).ready(function() {
-	
+
   $("a[href='portfolio.html']").on('click', function(event) {
-		$('.home').remove();
-		event.preventDefault();
-		$(".belowHead").load('portfolio.html .portfolio');
-	});
-	
-	$("a[href='contact.html']").on('click', function(event) {
-		$('.home').remove();
-		event.preventDefault();
-		$(".belowHead").load('contact.html .contact');
-	});
-	
-	$("a[href='index.html']").on('click', function(event) {
-		$('.home').remove();
-		event.preventDefault();
-		$(".belowHead").load('index.html .home');
-	});
+    $('.home').remove();
+    event.preventDefault();
+    $(".belowHead").load('portfolio.html .portfolio');
+  });
 
-		$('a.gallery').featherlightGallery({
-    previousIcon: '&#10092',
-    nextIcon: '&#10093',
-    galleryFadeIn: 300,
+  $("a[href='contact.html']").on('click', function(event) {
+    $('.home').remove();
+    event.preventDefault();
+    $(".belowHead").load('contact.html .contact');
+  });
 
-    openSpeed: 300
-	});
-	
-	$('.my-gallery').featherlightGallery({
-    filter: "a",
-    afterContent: function() {
-		this.$legend = this.$legend || $('<div class="legend"/>').insertAfter(this.$content);
-		this.$legend.text(this.$currentTarget.attr('alt'));
-		}
-	});
-		$.backstretch([
-		"img/Digital/Portraits/rick.png",
-		"img/Digital/Portraits/tayah.png",
-		"img/Digital/Portraits/tyrion.png"
-		], {duration: 4000, fade: 750});
+  $("a[href='index.html']").on('click', function(event) {
+    $('.home').remove();
+    event.preventDefault();
+    $(".belowHead").load('index.html .home');
+  });
+
+  $("a[href='about.html']").on('click', function(event) {
+    $('.home').remove();
+    event.preventDefault();
+    $(".belowHead").load('about.html .about');
+  });
+  $('a.gallery').featherlightGallery({
+    previousIcon : '&#10092',
+    nextIcon : '&#10093',
+    galleryFadeIn : 300,
+
+    openSpeed : 300
+  });
+
+  $('.my-gallery').featherlightGallery({
+    filter : "a",
+    afterContent : function() {
+      this.$legend = this.$legend || $('<div class="legend"/>').insertAfter(this.$content);
+      this.$legend.text(this.$currentTarget.attr('alt'));
+    }
+  });
+  $.backstretch([ "img/Test/test1.jpg", "img/Test/test2.jpg", "img/Test/test3.jpg", "img/Test/test4.jpg" ],
+                { duration : 5000, fade: 2000 });
 })
-
